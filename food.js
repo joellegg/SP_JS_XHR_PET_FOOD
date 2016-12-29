2d0f6c3dee7e24c5db91c362545a8840527227c3
joellegg <joellegg@bellsouth.net>
2016-12-29 13:35:24 -0600

    function to write cat food to HTML

--- food.js
+++ food.js
@@ -28,6 +28,33 @@ function dogFoodHTML() {
     $('#dog_food').html(dogFoodText);
 }
 
+function catFoodHTML() {
+    let catFoodText = '';
+
+    for (let i = 0; i < catFood.cat_brands.length; i++) {
+        let catBreeds = '';
+        for (let j = 0; j < catFood.cat_brands[i].breeds.length; j++) {
+            catBreeds += `${catFood.cat_brands[i].breeds[j]}, `
+        }
+
+        let catFoodTypesPrices = '';
+        for (let k = 0; k < catFood.cat_brands[i].types.length; k++) {
+            for (let l = 0; l < catFood.cat_brands[i].types[k].volumes.length; l++) {
+                catFoodTypesPrices += `${catFood.cat_brands[i].types[k].type}, ${catFood.cat_brands[i].types[k].volumes[l].name}, ${catFood.cat_brands[i].types[k].volumes[l].price} : `;
+            }
+
+        }
+        catFoodText += `
+            <tr>
+                <td>${catFood.cat_brands[i].name}</td>
+                <td>${catBreeds}</td>
+                <td>${catFoodTypesPrices}</td>
+            </tr>`;
+    }
+
+    $('#cat_food').html(catFoodText);
+}
+
 ///////////////////////////
 ///   Event Listeners   ///
 ///////////////////////////
@@ -38,11 +65,10 @@ function dogFoodHTML() {
 
 $.getJSON('JSON/dog-food.json', function(data) {
     dogFood = data;
-    console.log(dogFood)
     dogFoodHTML();
 });
 
 $.getJSON('JSON/cat-food.json', function(data) {
     catFood = data;
-    console.log(catFood)
+    catFoodHTML();
 });
